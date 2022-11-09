ae3caaa7aae56b2609182ffbebbdf07bc1af0efa
Arthur Honorio <hno.arthur@gmail.com>
2022-11-09 18:55:25 -0300

    fix: setLoggedUser function

--- src/states/loggedUser.tsx
+++ src/states/loggedUser.tsx
@@ -1,23 +1,25 @@
 import { DocumentData } from "firebase/firestore"
 import create from "zustand"
 
-export type userProps = {
-    displayName: string
-    photoURL: string | null
-    status: string
-    uid: string
-    email: string
-    contacts?: userProps[]
-} | DocumentData
+export type userProps =
+    | {
+          displayName: string
+          photoURL: string | null
+          status: string
+          uid: string
+          email: string
+          contacts?: userProps[]
+      }
+    | DocumentData
 
 type useLoggedUserStoreProps = {
     loggedUser: userProps | null
-    setLoggedUser: (user: userProps) => void
+    setLoggedUser: (user: userProps | null) => void
 }
 
 export const useLoggedUserStore = create<useLoggedUserStoreProps>(set => {
     return {
         loggedUser: null,
-        setLoggedUser: user => set(state => ({ loggedUser: user })),
+        setLoggedUser: user => set({ loggedUser: user }),
     }
 })
