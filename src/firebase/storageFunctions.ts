6c54068a278273209294ea7aa7824ca595da2275
Arthur Honorio <hno.arthur@gmail.com>
2022-11-26 11:35:25 -0300

    adding file size alert

--- src/firebase/storageFunctions.ts
+++ src/firebase/storageFunctions.ts
@@ -4,21 +4,29 @@ import {
     uploadBytesResumable,
     getDownloadURL,
 } from "firebase/storage"
+import { useSnackbarStore } from "../states/snackbar"
 import { app } from "./firebaseConfig"
 
 const storage = getStorage(app)
 
-export const uploadFiles = (
-    file: any,
-    callback: (url: string) => void
-) => {
+export const uploadFiles = async (file: any, callback: (url: string) => void) => {
+    if (+file.size > 1024 * 2) {
+        useSnackbarStore.setState({
+            open: true,
+            message: "Imagem deve ter no máximo 2mb",
+            type: "error",
+        })
+        return
+    }
     const fileName = `${file.lastModified}-${file.name}`
     const storageRef = ref(storage, fileName)
 
     const uploadTask = uploadBytesResumable(storageRef, file)
     uploadTask.on(
         "state_changed",
-        () => {},
+        e => {
+            console.log(e)
+        },
         err => {
             console.log(err.message)
             console.log(Object.entries(err))
