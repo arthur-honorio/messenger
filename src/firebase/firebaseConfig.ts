c297012717c363922e3114a086ceaa7ba57f1961
Arthur Honorio <hno.arthur@gmail.com>
2023-09-12 23:12:50 -0300

    Update firebaseConfig.ts

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBk8rE8nbIPFy-lhFsp9GV5NJyzmki-71Q",
-    authDomain: "rav-messenger.firebaseapp.com",
-    projectId: "rav-messenger",
-    storageBucket: "rav-messenger.appspot.com",
-    messagingSenderId: "783508704351",
-    appId: "1:783508704351:web:91164dfda8c9ee7bd09fea",
-    measurementId: "G-JZXQ87EHZ8",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
 }
 
 export const app = initializeApp(firebaseConfig)
